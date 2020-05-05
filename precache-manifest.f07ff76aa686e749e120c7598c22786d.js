d3f10192307ea64108df8ff50fe40c08e4bf01b6
nbgdeki <radic.dejan.nbg@gmail.com>
2020-05-05 16:10:45 +0200

    Updates

---
+++ precache-manifest.f07ff76aa686e749e120c7598c22786d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51736de1d9e16b912f76e98899994263",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "93c74b4177b2b453c434",
+    "url": "/monsters-rolodex/static/css/main.9c6fa49c.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/monsters-rolodex/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93c74b4177b2b453c434",
+    "url": "/monsters-rolodex/static/js/main.3910451d.chunk.js"
+  },
+  {
+    "revision": "60b3e6aee1ab568e197c",
+    "url": "/monsters-rolodex/static/js/runtime-main.ed829367.js"
+  }
+]);
